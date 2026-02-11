ed3c575b0a9d75ff194423fbf03c2c17fe81f537
Nikhil <nsarathy3@gmail.com>
2026-02-10 21:04:40 -0600

    Modified clamp function in customFunctions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/NikhilFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/NikhilFunctions.java
@@ -3,26 +3,22 @@ package org.firstinspires.ftc.teamcode.Util;
 
 import org.apache.commons.math3.util.FastMath;
 
+import dev.nextftc.core.commands.Command;
+
 public class NikhilFunctions {
-
-
 
     public double clamp(double x) {
 
         if (x > 1) {
             return 1;
         }
-
         else if (x < -1) {
             return -1;
         }
         else {
             return x;
         }
-
     }
-
-
     public double atan2(double deltaY, double deltaX) {
         return FastMath.atan2(deltaY, deltaX);
     }
