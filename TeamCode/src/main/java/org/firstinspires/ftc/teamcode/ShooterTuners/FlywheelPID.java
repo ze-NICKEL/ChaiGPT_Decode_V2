0d3d3e2902590433bc82ddbcbba3748209ed649e
Nikhil <nsarathy3@gmail.com>
2026-02-10 21:03:25 -0600

    Fixed turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTuners/FlywheelPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTuners/FlywheelPID.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.ShooterTuners;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ShooterSystems.ExtremeNikhilFlywheel;
 
+
+@Config
+
+@TeleOp(name = "FLYWHEEL PID TUNER", group = "tuning")
 public class FlywheelPID extends OpMode {
     ExtremeNikhilFlywheel flywheel;
 
